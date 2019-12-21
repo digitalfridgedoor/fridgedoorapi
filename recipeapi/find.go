363c37a1ee0d8874d60b22afe0a719347d635781
Maisie <maisiesadler@hotmail.com>
2019-12-21 19:28:47 +0000

    Add find recipe by name

--- recipeapi/find.go
+++ recipeapi/find.go
@@ -3,10 +3,22 @@ package recipeapi
 import (
 	"context"
 
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/digitalfridgedoor/fridgedoorapi/userviewapi"
 	"github.com/digitalfridgedoor/fridgedoordatabase/recipe"
 )
 
 // FindOne finds a recipe by id
 func FindOne(ctx context.Context, recipeID string) (*recipe.Recipe, error) {
 	return recipe.FindOne(ctx, recipeID)
+}
+
+// FindByName finds users recipes by name
+func FindByName(ctx context.Context, request *events.APIGatewayProxyRequest, searchTerm string) ([]*recipe.Recipe, error) {
+	view, err := userviewapi.GetOrCreateUserView(ctx, request)
+	if err != nil {
+		return make([]*recipe.Recipe, 0), err
+	}
+
+	return recipe.FindByName(ctx, searchTerm, view.ID)
 }
