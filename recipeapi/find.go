dc4bdf598ec435c4207f6b1b5b380740b2492245
Maisie <maisiesadler@hotmail.com>
2019-12-23 08:49:07 +0000

    Add custom type for recipeapi

--- recipeapi/find.go
+++ recipeapi/find.go
@@ -3,22 +3,61 @@ package recipeapi
 import (
 	"context"
 
-	"github.com/aws/aws-lambda-go/events"
+	"github.com/digitalfridgedoor/fridgedoordatabase/userview"
+
 	"github.com/digitalfridgedoor/fridgedoorapi/userviewapi"
 	"github.com/digitalfridgedoor/fridgedoordatabase/recipe"
+
+	"github.com/aws/aws-lambda-go/events"
 )
 
 // FindOne finds a recipe by id
-func FindOne(ctx context.Context, recipeID string) (*recipe.Recipe, error) {
-	return recipe.FindOne(ctx, recipeID)
+func FindOne(ctx context.Context, request *events.APIGatewayProxyRequest, recipeID string) (*Recipe, error) {
+	view, err := userviewapi.GetOrCreateUserView(ctx, request)
+	if err != nil {
+		return nil, err
+	}
+
+	recipe, err := recipe.FindOne(ctx, recipeID)
+	if err != nil {
+		return nil, err
+	}
+
+	return mapToDto(recipe, view), nil
 }
 
 // FindByName finds users recipes by name
-func FindByName(ctx context.Context, request *events.APIGatewayProxyRequest, searchTerm string) ([]*recipe.Recipe, error) {
+func FindByName(ctx context.Context, request *events.APIGatewayProxyRequest, searchTerm string) ([]*Recipe, error) {
 	view, err := userviewapi.GetOrCreateUserView(ctx, request)
 	if err != nil {
-		return make([]*recipe.Recipe, 0), err
+		return make([]*Recipe, 0), err
 	}
 
-	return recipe.FindByName(ctx, searchTerm, view.ID)
+	recipes, err := recipe.FindByName(ctx, searchTerm, view.ID)
+	if err != nil {
+		return nil, err
+	}
+
+	return mapToDtos(recipes, view), nil
+}
+
+func mapToDtos(r []*recipe.Recipe, view *userview.View) []*Recipe {
+	mapped := make([]*Recipe, len(r))
+	for idx, v := range r {
+		mapped[idx] = mapToDto(v, view)
+	}
+
+	return mapped
+}
+
+func mapToDto(r *recipe.Recipe, view *userview.View) *Recipe {
+	canEdit := r.AddedBy == view.ID
+	return &Recipe{
+		ID:        r.ID,
+		Name:      r.Name,
+		CanEdit:   canEdit,
+		Method:    r.Method,
+		Recipes:   r.Recipes,
+		ParentIds: r.ParentIds,
+	}
 }
---
+++ recipeapi/models.go
@@ -0,0 +1,16 @@
+package recipeapi
+
+import (
+	"github.com/digitalfridgedoor/fridgedoordatabase/recipe"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+// Recipe represents a recipe
+type Recipe struct {
+	ID        primitive.ObjectID   `json:"id" bson:"_id,omitempty"`
+	Name      string               `json:"name"`
+	CanEdit   bool                 `json:"canEdit"`
+	Method    []recipe.MethodStep  `json:"method"`
+	Recipes   []recipe.SubRecipe   `json:"recipes"`
+	ParentIds []primitive.ObjectID `json:"parentIds"`
+}
