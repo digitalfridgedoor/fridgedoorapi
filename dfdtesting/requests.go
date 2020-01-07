21ff2f0a0f957b56b05fabcd57feceb31e9bf185
Maisie <maisiesadler@hotmail.com>
2020-01-07 20:41:19 +0000

    Create and remove test user

--- dfdtesting/requests.go
+++ dfdtesting/requests.go
@@ -1,6 +1,12 @@
 package dfdtesting
 
-import "github.com/aws/aws-lambda-go/events"
+import (
+	"context"
+	"digitalfridgedoor/fridgedoorapi/fridgedoorgateway"
+	"digitalfridgedoor/fridgedoordatabase/userview"
+
+	"github.com/aws/aws-lambda-go/events"
+)
 
 // CreateTestAuthorizedRequest creates an authenticated api gateway request for the given user
 func CreateTestAuthorizedRequest(username string) *events.APIGatewayProxyRequest {
@@ -16,4 +22,22 @@ func CreateTestAuthorizedRequest(username string) *events.APIGatewayProxyRequest
 	}
 
 	return request
+}
+
+// CreateTestAuthenticatedUser creates a user view and AuthenticatedUser for the given user
+func CreateTestAuthenticatedUser(username string) *fridgedoorgateway.AuthenticatedUser {
+	request := CreateTestAuthorizedRequest(username)
+
+	user, err := fridgedoorgateway.GetOrCreateAuthenticatedUser(context.TODO(), request)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return user
+}
+
+// DeleteTestUser deletes a user
+func DeleteTestUser(user *fridgedoorgateway.AuthenticatedUser) {
+	userview.Delete(context.TODO(), user.Username)
 }
