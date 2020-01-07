8da0c5118196dbafade63f0d0bb0e28f83ee3f60
Maisie <maisiesadler@hotmail.com>
2020-01-07 19:54:11 +0000

    Create api gateway helper lib

--- userviewapi/userview.go
+++ userviewapi/userview.go
@@ -2,38 +2,10 @@ package userviewapi
 
 import (
 	"context"
-	"fmt"
 
-	"github.com/digitalfridgedoor/fridgedoorapi"
 	"github.com/digitalfridgedoor/fridgedoordatabase/recipe"
 	"github.com/digitalfridgedoor/fridgedoordatabase/userview"
-
-	"github.com/aws/aws-lambda-go/events"
 )
-
-// GetOrCreateUserView creates a new UserView for the logged in user
-func GetOrCreateUserView(ctx context.Context, request *events.APIGatewayProxyRequest) (*userview.View, error) {
-
-	username, ok := fridgedoorapi.ParseUsername(request)
-	if !ok {
-		return nil, errNotLoggedIn
-	}
-
-	view, err := userview.GetByUsername(ctx, username)
-	if err != nil {
-		view, err = userview.Create(ctx, username)
-	}
-
-	nickname, ok := fridgedoorapi.ParseNickname(request)
-	if ok {
-		fmt.Printf("Got nickname: %v\n", nickname)
-		err = userview.SetNickname(ctx, view, nickname)
-		if err != nil {
-			fmt.Printf("Error setting nickname: %v\n", err)
-		}
-	}
-	return view, nil
-}
 
 // GetUserViewByID gets a userview by id
 func GetUserViewByID(ctx context.Context, userviewID string) (*View, error) {
