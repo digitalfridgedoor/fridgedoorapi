19cfe7aaf46d1d5bd69497ad9185c869271204ee
Maisie <maisiesadler@hotmail.com>
2020-01-11 11:00:59 +0000

    Add GetPublicRecipes

--- linkeduserapi/find.go
+++ linkeduserapi/find.go
@@ -10,6 +10,24 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
+// GetPublicRecipes returns the public recipes user views for all users
+func GetPublicRecipes(ctx context.Context) ([]*LinkedUser, error) {
+	userViews, err := userview.GetLinkedUserViews(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	populated := make([]*LinkedUser, len(userViews))
+	for idx, uv := range userViews {
+		linkedUser, err := populatePublicUser(ctx, uv)
+		if err != nil {
+			populated[idx] = linkedUser
+		}
+	}
+
+	return populated, nil
+}
+
 // GetOtherUsersRecipes returns a collection of user views for all users
 func GetOtherUsersRecipes(ctx context.Context, user *fridgedoorgateway.AuthenticatedUser) ([]*LinkedUser, error) {
 	userViews, err := userview.GetLinkedUserViews(ctx)
@@ -41,6 +59,19 @@ func populateLinkedUser(ctx context.Context, user *fridgedoorgateway.Authenticat
 	}, nil
 }
 
+func populatePublicUser(ctx context.Context, view *userview.View) (*LinkedUser, error) {
+	recipes, err := findPublicRecipes(ctx, view.ID)
+	if err != nil {
+		return nil, err
+	}
+
+	return &LinkedUser{
+		ID:       view.ID,
+		Nickname: view.Nickname,
+		Recipes:  recipes,
+	}, nil
+}
+
 func findRecipes(ctx context.Context, user *fridgedoorgateway.AuthenticatedUser, linkedUserID primitive.ObjectID) ([]*Recipe, error) {
 
 	recipes, err := recipe.FindByTags(ctx, linkedUserID, []string{}, []string{})
@@ -48,15 +79,34 @@ func findRecipes(ctx context.Context, user *fridgedoorgateway.AuthenticatedUser,
 		return nil, err
 	}
 
-	return mapToDtos(recipes, user), nil
+	return mapToAuthedDtos(recipes, user), nil
 }
 
-func mapToDtos(r []*recipe.Recipe, user *fridgedoorgateway.AuthenticatedUser) []*Recipe {
+func findPublicRecipes(ctx context.Context, linkedUserID primitive.ObjectID) ([]*Recipe, error) {
+
+	recipes, err := recipe.FindPublic(ctx, linkedUserID)
+	if err != nil {
+		return nil, err
+	}
+
+	return mapToDtos(recipes), nil
+}
+
+func mapToAuthedDtos(r []*recipe.Recipe, user *fridgedoorgateway.AuthenticatedUser) []*Recipe {
 	mapped := []*Recipe{}
-	for idx, v := range r {
+	for _, v := range r {
 		if v.CanView(user.ViewID) {
-			mapped[idx] = mapToDto(v)
+			mapped = append(mapped, mapToDto(v))
 		}
+	}
+
+	return mapped
+}
+
+func mapToDtos(r []*recipe.Recipe) []*Recipe {
+	mapped := []*Recipe{}
+	for _, v := range r {
+		mapped = append(mapped, mapToDto(v))
 	}
 
 	return mapped
