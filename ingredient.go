fdec72144a240229db944687c2a13d1358340d63
Maisie <maisiesadler@hotmail.com>
2020-05-02 21:53:34 +0100

    fix build

--- ingredient.go
+++ ingredient.go
@@ -3,15 +3,17 @@ package fridgedoorapi
 import (
 	"context"
 
+	"github.com/digitalfridgedoor/fridgedoordatabase/dfdmodels"
+
 	"github.com/digitalfridgedoor/fridgedoordatabase/ingredient"
 )
 
 // CreateIngredient creates a new ingredient
-func CreateIngredient(name string) (*ingredient.Ingredient, error) {
+func CreateIngredient(name string) (*dfdmodels.Ingredient, error) {
 	return ingredient.Create(context.Background(), name)
 }
 
 // SearchIngredients retrieves the ingredients matching the query
-func SearchIngredients(startsWith string) ([]*ingredient.Ingredient, error) {
+func SearchIngredients(startsWith string) ([]*dfdmodels.Ingredient, error) {
 	return ingredient.FindByName(context.Background(), startsWith)
 }
