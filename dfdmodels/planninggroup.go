9ba38e83245e2e02635f6631b5a50e3a29c3af74
Maisie <maisiesadler@hotmail.com>
2020-07-31 20:09:32 +0100

    add  name

--- dfdmodels/planninggroup.go
+++ dfdmodels/planninggroup.go
@@ -7,5 +7,6 @@ import (
 // PlanningGroup represents a group of people who share a meal plan
 type PlanningGroup struct {
 	ID      *primitive.ObjectID  `json:"id" bson:"_id,omitempty"`
+	Name    string               `json:"name"`
 	UserIDs []primitive.ObjectID `json:"userIDs"`
 }
