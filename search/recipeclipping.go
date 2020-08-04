f3810df818e8ffc9a0b34e42aa416d1c25a4b1b8
Maisie <maisiesadler@hotmail.com>
2020-08-04 22:07:42 +0100

    fix bugs

--- search/recipeclipping.go
+++ search/recipeclipping.go
@@ -31,13 +31,21 @@ func FindByName(ctx context.Context, startsWith string, userID primitive.ObjectI
 	}
 
 	for _, c := range clippings {
-		if _, ok := rids[*c.ID]; !ok {
+		canadd := true
+
+		if c.RecipeID != nil {
+			// don't add a clipping that has been created into a recipe
+			if _, ok := rids[*c.RecipeID]; ok {
+				canadd = false
+			}
+		}
+
+		if canadd {
 			results = append(results, Result{
 				ClippingID: c.ID,
 				Name:       c.Name,
 			})
 		}
-
 	}
 
 	return results, nil
